01c9abc3ac908f5536930bc93cece5f090fdb6d6
TcnCunha <64099736+Tcncunha@users.noreply.github.com>
2025-07-22 14:40:55 +0200

    Extension to Make it Snow
    Extension to Make it Snow fun for the Xmas!

---
+++ Snow/SnowEffect.js
@@ -0,0 +1,25 @@
+define(["qlik", "css!./SnowEffect.css"], function (qlik) {
+    return {
+        paint: function ($element) {
+            function createSnowflake() {
+                const snowflake = document.createElement('div');
+                snowflake.classList.add('snowflake');
+                snowflake.textContent = '❄';
+                console.log('JavaScript está funcionando!!!');
+
+                $element.append(snowflake);
+
+                snowflake.style.left = `${Math.random() * window.innerWidth}px`;
+                snowflake.style.animationDuration = `${Math.random() * 3 + 2}s`;
+                snowflake.style.opacity = Math.random();
+                console.log('JavaScript está funcionando');
+
+                setTimeout(() => {
+                    snowflake.remove();
+                }, 5000);
+            }
+
+            setInterval(createSnowflake, 100);
+        }
+    };
+});
